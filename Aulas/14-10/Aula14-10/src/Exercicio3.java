49e064c085059ef071eee9b217f6de59b4fbaeb5
Fernanda <fernandaruaro26@hotmail.com>
2021-10-15 14:26:50 -0300

    update Exercicio3.java

--- Aulas/14-10/Aula14-10/src/Exercicio3.java
+++ Aulas/14-10/Aula14-10/src/Exercicio3.java
@@ -1,12 +1,22 @@
 //3. Criar um programa que receba uma palavra e em seguida imprime no console letra por letra.
 
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class Exercicio3 {
 
 	public static void main(String[] args) {
 		
+		Scanner scan = new Scanner(System.in);
 		
+		System.out.println("Digite uma palavra:");
+		String palavra = scan.nextLine();
+		scan.close();
+		
+		String letra[] = palavra.split("");
+		
+		for(int i=0; i<palavra.length(); i++) {
+			System.out.println(letra[i]);
+		}
 		
 	}
 
