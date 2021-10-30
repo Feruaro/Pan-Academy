f54acecfbcbd6dae8c18dc93c14a723baa26d6ec
Fernanda <fernandaruaro26@hotmail.com>
2021-10-30 12:23:00 -0300

    codigo tabuada- tratamento de excessões

--- Aulas/30-10/Tabuada-30-10/src/TabuadaTratamentoException.java
+++ Aulas/30-10/Tabuada-30-10/src/TabuadaTratamentoException.java
@@ -5,41 +5,34 @@ public class TabuadaTratamentoException {
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		Integer op;
 		
-		System.out.println("----- Menu Tabuada -----" +
-						 "\n1- Calcular tabuada" +
-						 "\n2- Sair" +
-						 "\nDigite sua opção: ");		
-
-		try {
-			op = scan.nextInt();
-			if(op == 1) {
-				calcularTabuada(scan);
-			}
-		} catch (InputMismatchException ime) {
-			System.err.println("Digite um número");
-		} finally {
-			scan.close();
-		}	
+		while(true) {
+			System.out.print("\n----- Menu Tabuada -----" +
+					 		 "\n1- Calcular tabuada" +
+					         "\n2- Sair" +
+					         "\nDigite sua opção: ");
+			try {
+				Integer op = scan.nextInt();
+				if(op == 1) {
+					System.out.print("\nDigite o número que deseja calcular a tabuada: ");
+					Integer num = scan.nextInt();
+					System.out.println("\n----- Tabuada do " + num + " -----\n");
+					for(int i=1; i<=10; i++) {
+						System.out.println(i + " x " + num + " = " +(i*num));
+					}
+				} else if(op == 2) {
+					break;
+				} else {
+					System.out.println("Opção inválida! Tente novamente!");
+				}
+			} catch (InputMismatchException ime) {
+				System.err.println("Digite um número");
+				scan.next();     //limpar o buffer 
+			} 
+		}
 		
-				
-	}
-
-	private static void calcularTabuada(Scanner scan) {
-		System.out.println("Digite o número que deseja calcular a tabuada: ");
-		Integer num;
-		try {
-			num = scan.nextInt();
-			System.out.println("----- Tabuada do " + num + " -----\n\n");
-			for(int i=1; i<=10; i++) {
-				System.out.println(i + " x " + num + " = " +(i*num));
-			}
-		} catch (InputMismatchException ime) {
-			System.out.println("Informe um número");
-		}				
-		
-	}
-	
+		scan.close();
+						
+	}	
 	
 }
